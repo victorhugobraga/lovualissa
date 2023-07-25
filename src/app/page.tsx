e962d083a70c56fead8f5864ce2a1a2fed10c8d2
Victor <victor.braga@openk.com.br>
2023-07-25 00:21:05 -0300

    template incial

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,169 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import React from "react";
 
 export default function Home() {
+  // create a modal that open when click on the card
+  const [modal, setModal] = React.useState(false);
+  const [modalContent, setModalContent] = React.useState({
+    title: "",
+    description: "",
+  });
+
+  const handleModal = (title: string, description: string) => {
+    setModalContent({
+      title,
+      description,
+    });
+    setModal(true);
+  };
+
+  const closeModal = () => {
+    setModal(false);
+  };
+
+  const frasesJson = [
+    {
+      categoria: "Para a gata mais inteligente que existe!!",
+      cantadas: [
+        {
+          titulo: "Deixa eu estudar sua língua",
+          frase:
+            "pesquisas apontam que agente junto é erro de gramática, mas a gente separado é erro do destino. ",
+        },
+        {
+          titulo: "Acho que tá rolando uma química aqui",
+          frase: "Não sou adenina mas sou louco por timina.",
+        },
+        {
+          titulo: "Mais atrativa que carga negativa pela positiva",
+          frase:
+            "Me chama de tabela Periódica e diz que rolou uma química entre nós.",
+        },
+        {
+          titulo: "Única história que quero saber é a nossa",
+          frase: "fazer ENEM? Só se for pra entrar no seu coração. ",
+        },
+        {
+          titulo: "Você é mais atrativa que a Terra",
+          frase:
+            "Nosso amor deve ser de outro mundo, pq mesmo fora da gravidade da Terra, eu ficaria caidinho por vc...",
+        },
+        {
+          titulo: "Única química que quero saber, é a nossa",
+          frase:
+            "Queria ser um gás nobre, mas um gás nobre eu não posso ser. Porque um gás nobre se estabiliza com 8 elétrons, e eu me estabilizo só com vc.",
+        },
+      ],
+    },
+    {
+      categoria: "Não é sobre ser brega, mas é que te amo dms",
+      cantadas: [
+        {
+          titulo: "Seu pai é padeiro?",
+          frase:
+            "Me passa seu Instagram? É que a minha mãe falou para eu seguir o meu sonho.",
+        },
+        {
+          titulo: "Já vai no Maps que eu quero cada destino com você",
+          frase: "Você não é GPS quebrado, mas me deixa sem rumo.",
+        },
+        {
+          titulo: "Inclusive, você é meu primeiro ponto vou para qlqr dúvida",
+          frase: "Seu nome é Google? Porquê você tem tudo que eu procuro.",
+        },
+        {
+          titulo: "Tem que pegar fila, aproveita que é exclusiva para você ;)",
+          frase:
+            "Onde é que eu deixo o meu currículo para concorrer à vaga de amor da sua vida?",
+        },
+        {
+          titulo: "Como eu facilmente passaria noite após noite te admirando",
+          frase:
+            "Se as estrelas fossem tão lindas quanto você, eu passaria noites em claro olhando para elas.",
+        },
+        {
+          titulo: "Eu não era seu tudo?? aff...",
+          frase: "Se nada dura para sempre, quer ser meu nada?",
+        },
+        {
+          titulo: "Vi nem o tempo passar, tava olhando para você",
+          frase:
+            "Um mês atrás eu era apaixonado(a) por você. E parece que estamos no mês passado ainda.",
+        },
+        {
+          titulo:
+            "Te acompanhar em tudo quanto é canto nunca foi tão estratégico",
+          frase:
+            "Você acredita em amor a primeira vista? Ou eu devo passar aqui de novo",
+        },
+        {
+          titulo: "Me chama de Beijamim...",
+          frase:
+            "Queria ser um beija-flor, mas beija-flor não posso ser, beija-flor beija várias flores, e eu só quero beijar você",
+        },
+        {
+          titulo: "Sentindo uma mistura de sentimentos aqui",
+          frase:
+            "Você nunca poderia ser anestesista. Porque é impossível não sentir nada perto de você.",
+        },
+        {
+          titulo: "Aliança é um bom ponto inclusive, com o seu nome ent! <3",
+          frase:
+            "Eu não quero seu dinheiro, nem sua herança, eu apenas quero o seu nome, gravado na minha aliança.",
+        },
+        {
+          titulo: "Vamo ter que ir para RJ gata",
+          frase:
+            "A cidade maravilhosa só se chama Rio de Janeiro pq vc não tinha nascido ainda.",
+        },
+      ],
+    },
+  ];
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="p-5">
+      <h1>Lov U Alissa</h1>
+      <div className="flex flex-col min-h-screen py-2 w-max">
+        <div className="flex flex-wrap justify-around max-w-4xl mt-6 sm:w-full">
+          {frasesJson.map((frase) => (
+            <div
+              className="p-2 m-2 bg-pink-800 rounded-xl shadow-md hover:shadow-lg"
+              key={frase.cantadas[0].titulo}
+            >
+              <h2 className="text-xl font-bold text-center">
+                {frase.categoria}
+              </h2>
+              <div className="flex flex-col gap-4">
+                {frase.cantadas.map((cantada) => (
+                  <div
+                    className="flex flex-col bg-pink-900 p-3 rounded-lg"
+                    key={cantada.titulo}
+                    onClick={() => handleModal(cantada.titulo, cantada.frase)}
+                  >
+                    {/* <h3 className="text-lg font-bold">{cantada.titulo}</h3> */}
+                    <p className="">{cantada.frase}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {modal && modalContent.description && (
+        <div className="bg-red-900 fixed top-1/2 left-1/2 -translate-x-1/2 p-4 w-[80vw] rounded-lg flex gap-4 flex-col z-10">
+          <h2 className="text-center text-lg">{modalContent.title}</h2>
+          <p>{modalContent.description}</p>
+        </div>
+      )}
+      {/* make a blur background for the modal, and when i click on it, the modal will close */}
+      {modal && (
+        <div
+          className="fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50"
+          onClick={closeModal}
+        ></div>
+      )}
     </main>
-  )
+  );
 }
